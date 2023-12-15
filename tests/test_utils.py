8e1afd57833f6a0f36c3f954cf97be4ca8817214
Layik Hama <layik.hama@gmail.com>
2023-12-15 19:24:25 +0000

    tests: Windows os.remove error

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,4 @@
 import pytest
-import tempfile
 import os
 from vizdataquality.utils import init_logging, end_logging
 
@@ -14,18 +13,17 @@ def test_create_new_log_file(tmp_path):
         assert expected_header in header
 
 def test_end_logging():
-    with tempfile.NamedTemporaryFile(delete=False) as temp_log:
-        log_file = temp_log.name
-
+    log_file = 'test_log.log'
     log, handlers = init_logging(log_file, True)
     initial_handler_count = len(log.handlers)
 
     end_logging(log, handlers)
-    # Close all handlers and clear them
-    for handler in log.handlers:
-        handler.close()
-    log.handlers.clear()
+    # Check if handlers added in this test are removed
+    assert len(log.handlers) == initial_handler_count - len(handlers)
 
     if os.path.exists(log_file):
-        os.remove(log_file)
+        try:
+            os.remove(log_file)
+        except PermissionError as e:
+            print(f"Error removing log file: {e}")
 
